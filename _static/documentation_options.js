b3a2bd480d395636f56d0716c36ac0ff6ee9ddf5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-02 09:12:48 +0000

    Deploy SmileiPIC/Smilei to github.com/SmileiPIC/Smilei.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v4.1-299-gaf2641ed',
+    VERSION: 'v4.1-300-g875dccd5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
